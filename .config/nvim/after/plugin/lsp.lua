6349d4769c0f902e27086d0bf4a7ea6f918a721e
Binary <49949198+BinaryAlien@users.noreply.github.com>
2024-04-13 07:39:09 +0000

    revert(neovim): remove 'ray-x/lsp_signature'
    This reverts commit 67b64d2c760f1bea0e3cc67767e1db0789706f0e.

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -1,14 +1,10 @@
 local lsp_zero = require('lsp-zero')
-local lsp_signature = require('lsp_signature')
-local lsp_signature_config = {bind = true, hint_prefix = ""}
 
 lsp_zero.on_attach(function(client, bufnr)
   lsp_zero.default_keymaps({buffer = bufnr})
-  lsp_signature.on_attach(lsp_signature_config, bufnr)
 end)
 
 lsp_zero.setup_servers({'ccls', 'nil_ls', 'pyright', 'tsserver'})
-lsp_signature.setup(lsp_signature_config)
 
 require('lspconfig').rust_analyzer.setup({
   settings = {
